f4fb11ed79605dd28a89f3d3ebc4a0c61cf9db62
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-03-29 01:28:37 -0400

    refactoring

--- main.py
+++ main.py
@@ -2,12 +2,10 @@
 import re
 import os
 import logging
-import json
-from argparse import ArgumentParser
 
-import requests
 import telebot
 
+from .finportbotutil.tipcalc import get_tipargparser
 
 logging.basicConfig(level=logging.INFO)
 
@@ -34,14 +32,6 @@ def sayonara(message):
     bot.send_message(message.chat.id, "Have a nice day!")
 
 
-def get_tipargparser():
-    tipparser = ArgumentParser(description='Tip Calculator')
-    tipparser.add_argument('subtotal', type=float)
-    tipparser.add_argument('state', type=str)
-    tipparser.add_argument('--split', type=int, default=1)
-    return tipparser
-
-
 @bot.message_handler(regexp=r'([tT]ip[s]?)(\s+)([\d]+[\.]?[\d+]?)(\s+)([A-Za-z]+)(\s+\d+)?')
 def calculate_tips(message):
     logging.info(message)
@@ -54,20 +44,8 @@ def calculate_tips(message):
         bot.send_message(message.chat.id, 'Wrong tip calculator arguments!')
         return
 
-    url = "https://1j79chd1w8.execute-api.us-east-1.amazonaws.com/default/Ingram1623TipCalculator"
-    payload = json.dumps({
-        "subtotal": args.subtotal,
-        "state": args.state,
-        "split": args.split
-    })
-    headers = {
-        'Content-Type': 'application/json',
-        'X-Amz-Content-Sha256': 'beaead3198f7da1e70d03ab969765e0821b24fc913697e929e726aeaebf0eba3',
-        'X-Amz-Date': '20220323T032656Z',
-        'Authorization': 'AWS4-HMAC-SHA256 Credential=AKIAV6PASP2CH24VHO3V/20220323/us-east-1/execute-api/aws4_request, SignedHeaders=content-type;host;x-amz-content-sha256;x-amz-date, Signature=d850ee5c12f2c3df740dd211565c0a9709feb7989434eacb6f3a6c50db97f10b'
-    }
-    response = requests.request("GET", url, headers=headers, data=payload)
-    result = json.loads(response.text)
+    result = calculate_tips(args.subtotal, args.state, args.split)
+
     response_text = """
     Subtotal: ${:.2f}
     State: {}

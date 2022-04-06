5b6f407c64a5869bf4fc25506e674d4bab92c4c1
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-04-06 16:34:33 -0400

    pointed to the wrong API

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ def handling_stockcorrelation_message(message):
 
     # calculate
     print('{}, {} from {} to {}'.format(symbol1, symbol2, startdate, enddate))
-    results = asyncio.run(get_symbols_correlation(symbol1, symbol2, startdate, enddate, stockinfo_api_url))
+    results = asyncio.run(get_symbols_correlation(symbol1, symbol2, startdate, enddate, stockcorr_api_url))
 
     # wrangle message
     if 'message' in results.keys() and results['message'] == 'Internal server error':

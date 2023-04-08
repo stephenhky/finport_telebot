ecc6afa5a4b90a37e679ab15b4bcf00e4ebea341
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2023-04-08 01:06:42 -0400

    inline button, and properly handle callback query

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ CMD_MA200 = ['stockgma200']
 CMD_SP500_MA = ['sp500ma']
 CMD_NASDAQ_MA = ['nasdaqma']
 CMD_DJI_MA = ['djima']
+CMD_MAPLOT = ['maplot']
 
 
 # polling flag
@@ -97,21 +98,21 @@ keyboard_indices = {
 def makeMAKeyboard():
     markup = telebot.types.InlineKeyboardMarkup()
 
-    for key, value in keyboard_indices.index():
+    for key, value in keyboard_indices.items():
         markup.add(
-            telebot.types.InlineKeyboardButton(text=value, callback_data='buttonma_{}'.format(key))
+            telebot.types.InlineKeyboardButton(text=value, callback_data='button_maplot_{}'.format(key))
         )
 
     return markup
 
 
-@bot.message_handler(commands=['maplot'])
+@bot.message_handler(commands=CMD_MAPLOT)
 def display_ma_keyboard(message):
     msg = bot.send_message(chat_id=message.chat.id,
-                     text='Choose one indices (xxxxxdddddd)',
+                     text='Choose one index',
                      reply_markup=makeMAKeyboard(),
                      parse_mode='HTML')
-    bot.register_next_step_handler(msg, handle_query)
+    bot.register_next_step_handler(msg, handle_maplot_callback_query)
 
 
 @bot.message_handler(commands=CMD_START)
@@ -454,17 +455,16 @@ def sending_index_ma(message):
     }
 
 
-# @bot.callback_query_handlers(func=lambda call: call.data.startswith('buttonma_'))
-def handle_query(call):
+def handle_maplot_callback_query(call):
     callbackstr = call.data
 
-    if callbackstr == 'buttonma_sp500':
+    if callbackstr == 'button_maplot_sp500':
         index = '^GSPC'
         plottitle = 'S&P 500 (^GSPC)'
-    elif callbackstr == 'buttonma_nasdaq':
+    elif callbackstr == 'button_maplot_nasdaq':
         index = '^IXIC'
         plottitle = 'NASDAQ (^IXIC)'
-    elif callbackstr == 'buttonma_dji':
+    elif callbackstr == 'button_maplot_dji':
         index = '^DJI'
         plottitle = 'Dow Jones (^DJI)'
     else:
@@ -472,7 +472,7 @@ def handle_query(call):
 
     plot_info = plotting_index_ma(index, plottitle)
     f = urllib.request.urlopen(plot_info['plot']['url'])
-    bot.send_photo(call.id, f)
+    bot.send_photo(call.from_user.id, f)
     return {
         'ploturl': plot_info['plot']['url']
     }
@@ -493,26 +493,36 @@ def lambda_handler(event, context):
         update = telebot.types.Update.de_json(message)
         logging.info(update)
         print(update)
-        message = update.message
-        try:
-            add_modify_user(message)
-        except AttributeError:
-            pass
+        if update.message is not None:
+            message = update.message
+            try:
+                add_modify_user(message)
+            except AttributeError:
+                pass
 
-        try:
-            bot.process_new_messages([message])
-            print('Processed.')
+            try:
+                bot.process_new_messages([message])
+                print('Processed.')
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps({'approach': 'webhook'})
+                }
+            except AttributeError:
+                print('Telegram error.', file=sys.stderr)
+                print(traceback.format_exc(), file=sys.stderr)
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps({'approach': 'webhook'})
+                }
+        elif update.callback_query is not None:
+            callback_cmd = update.callback_query.data
+            if callback_cmd.startswith('button_maplot_'):
+                handle_maplot_callback_query(update.callback_query)
             return {
                 'statusCode': 200,
                 'body': json.dumps({'approach': 'webhook'})
             }
-        except AttributeError:
-            print('Telegram error.', file=sys.stderr)
-            print(traceback.format_exc(), file=sys.stderr)
-            return {
-                'statusCode': 200,
-                'body': json.dumps({'approach': 'webhook'})
-            }
+
 
 
 if __name__ == '__main__':
